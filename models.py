cac150e21509c1c3142fb725181a569bebf0c88f
octex <56984557+octex@users.noreply.github.com>
2022-02-17 00:05:13 -0300

    Added new models
    Modified Block class model and added new base sprites

--- models.py
+++ models.py
@@ -1,13 +1,51 @@
 
 
+class Blocks:
+    BASE = "#"
+
+    I = (" _\n|_|\n|_|\n|_|\n|_|",
+    " _ _ _ _\n|_|_|_|_|")
+
+    O = (" _ _\n|_|_|\n|_|_|")
+    
+    T = ("   _\n _|_|_\n|_|_|_|\n",
+        " _\n|_|_\n|_|_|\n|_|",
+        " _ _ _\n|_|_|_|\n  |_|",
+        "   _\n _|_|\n|_|_|\n  |_|")
+    
+    S = ("   _ _\n _|_|_|\n|_|_|\n",
+        " _\n|_|_\n|_|_|\n  |_|")
+
+    L = (" _\n|_|\n|_|_\n|_|_|",
+        " _ _ _\n|_|_|_|\n|_|\n",
+        " _ _\n|_|_|\n  |_|\n  |_|",
+        "     _\n _ _|_|\n|_|_|_|")
+
+    Z = (" _ _\n|_|_|_\n  |_|_|",
+        "   _\n _|_|\n|_|_|\n|_|")
+
+    J = ("   _\n  |_|\n _|_|\n|_|_|",
+        " _\n|_|_ _\n|_|_|_|",
+        " _ _\n|_|_|\n|_|\n|_|",
+        " _ _ _\n|_|_|_|\n    |_|")
+
+    ALL = [I, O, T, S, L, Z, J]
+
+
 class Block:
-    def __init__(self, x, y, idle):
+    def __init__(self, x, y, animations):
         self.x = x
         self.y = y
-        self.idle = idle
-    
-    def get_rotation(self):
-        pass
-    
+        self.animations = animations
+        self.current_index = 0
+        self.sprite = self.animations[self.current_index]
+
     def get_idle(self):
         return self.idle
+
+    def rotate(self):
+        if self.current_index == len(self.animations):
+            self.current_index = 0
+        else:
+            self.current_index += 1
+        self.sprite = self.animations[self.current_index]
