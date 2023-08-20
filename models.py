9d1e679ba86bfce16de8adf0bcfe31072194d685
octex <octavio.gurnik@gmail.com>
2023-08-20 15:31:35 -0300

    feat: Added sub-class for unicode colors

--- models.py
+++ models.py
@@ -3,6 +3,17 @@
 class Constants:
     HIGH_FRAMES = 0.5
     LOW_FRAMES = 0.15
+    
+    class BColors:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKCYAN = '\033[96m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
 
 class Blocks:
     BASE = "#"
