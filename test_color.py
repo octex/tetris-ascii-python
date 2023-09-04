762324c6b37339679c8c1cd51ddd65e78192797a
octex <octavio.gurnik@gmail.com>
2023-09-04 13:00:11 -0300

    feat: Added more txt to color palette test

--- test_color.py
+++ test_color.py
@@ -7,6 +7,9 @@ def main(stdscr):
         curses.init_pair(i, i, -1)
     try:
         for i in range(0, 255):
+            stdscr.addstr('█', curses.color_pair(i))
+        stdscr.addstr('\n\nIDs:\n\n')
+        for i in range(0, 255):
             stdscr.addstr(str(i), curses.color_pair(i))
     except curses.ERR:
         # End of screen reached
